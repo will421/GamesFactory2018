090d16c09049d9540a6b5e1d456983afedc4fd29
WB <william.bobo@hardis.fr>
2018-07-27 23:52:22 +0200

    Init project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public float rotationSpeed;
+	public GameObject[] levels;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed * Input.GetAxis("Horizontal"));
+	}
+}
